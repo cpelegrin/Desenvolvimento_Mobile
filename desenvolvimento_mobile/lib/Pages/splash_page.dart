80c72364b88c2c9463cf513fa3e086ba7f38fa13
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2021-09-07 16:30:58 -0300

    Unidade 4 Aula 6 - Firebase - SplashScreen

---
+++ desenvolvimento_mobile/lib/Pages/splash_page.dart
@@ -0,0 +1,60 @@
+import 'package:desenvolvimento_mobile/Pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _SplashPage createState() => _SplashPage();
+}
+
+class _SplashPage extends State<SplashPage> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print(snapshot.error);
+          return Container(
+            color: Colors.white,
+            child: Text("Deu bão não!\n${snapshot.error}"),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          Future.delayed(Duration(seconds: 2)).then((_) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return HomePage();
+                },
+              ),
+            );
+          });
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container(
+          color: Colors.white,
+          child: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FlutterLogo(
+                size: 450,
+              ),
+              CircularProgressIndicator(),
+            ],
+          )),
+        );
+      },
+    );
+  }
+}
--- desenvolvimento_mobile/lib/main.dart
+++ desenvolvimento_mobile/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:desenvolvimento_mobile/Pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 import 'Pages/home_page.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
           primaryColor: Colors.pink[400],
           primaryColorDark: Colors.pink[900],
           accentColor: Colors.green[500]),
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
